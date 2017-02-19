de1d28cca4f6fda0efd20cc57613b03485213a11
madHatter106 <jkomd.283.2112@gmail.com>
2017-02-19 10:42:45 -0500

    Nikola auto commit.
    Nikola version: 7.8.1

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Erdem Karakoylu"  # (translatable)
-BLOG_TITLE = "Erdem's HmmBlog"  # (translatable)
+BLOG_TITLE = "Erdem K's DS corner"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://madhatter106.github.io/"
@@ -1121,19 +1121,19 @@ SOCIAL_BUTTONS_CODE = """
 # """ % SITE_URL
 #
 # If you prefer a Google search form, here's an example that should just work:
-# SEARCH_FORM = """
-# <!-- Google custom search -->
-# <form method="get" action="https://www.google.com/search" class="navbar-form navbar-right" role="search">
-# <div class="form-group">
-# <input type="text" name="q" class="form-control" placeholder="Search">
-# </div>
-# <button type="submit" class="btn btn-primary">
-# 	<span class="glyphicon glyphicon-search"></span>
-# </button>
-# <input type="hidden" name="sitesearch" value="%s">
-# </form>
-# <!-- End of custom search -->
-# """ % SITE_URL
+SEARCH_FORM = """
+<!-- Google custom search -->
+<form method="get" action="https://www.google.com/search" class="navbar-form navbar-right" role="search">
+<div class="form-group">
+<input type="text" name="q" class="form-control" placeholder="Search">
+</div>
+<button type="submit" class="btn btn-primary">
+ 	<span class="glyphicon glyphicon-search"></span>
+ </button>
+ <input type="hidden" name="sitesearch" value="%s">
+ </form>
+ <!-- End of custom search -->
+ """ % SITE_URL
 
 # Use content distribution networks for jQuery, twitter-bootstrap css and js,
 # and html5shiv (for older versions of Internet Explorer)
